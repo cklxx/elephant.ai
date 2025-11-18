fd57191c7f020d407854f2b45ca25fd848fec97c
cklxx <1293822641@qq.com>
2025-11-18 17:50:41 +0800

    Prevent subagents from invoking phase delegates

--- internal/toolregistry/registry.go
+++ internal/toolregistry/registry.go
@@ -169,8 +169,14 @@ func (w *idAwareExecutor) Mode() tools.ExecutionMode {
 // This prevents nested subagent calls at registration level
 func (r *Registry) WithoutSubagent() ports.ToolRegistry {
 	return &filteredRegistry{
-		parent:  r,
-		exclude: map[string]bool{"subagent": true},
+		parent: r,
+		exclude: map[string]bool{
+			"subagent": true,
+			"explore":  true,
+			"code":     true,
+			"research": true,
+			"build":    true,
+		},
 	}
 }
 
